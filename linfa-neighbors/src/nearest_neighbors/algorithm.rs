e339c6b24aaf31a92664e4c3b4eb44e7a0f3ce98
Mason Stallmo <masonstallmo@gmail.com>
2019-12-17 22:41:07 -0700

    Complete making the algorithm generic over the input type and dimension.
    Got some help on the final implementation from the fine people over at r/rust.
    TLDR; I had missed on how to implement the nested trait bound on the NearestNeighbor struct.
    Link to the post is here: https://www.reddit.com/r/rust/comments/ec71n7/help_with_generic_trait_weirdness/

--- linfa-neighbors/src/nearest_neighbors/algorithm.rs
+++ linfa-neighbors/src/nearest_neighbors/algorithm.rs
@@ -1,7 +1,9 @@
 use ndarray::prelude::*;
 use ndarray::{Data, RemoveAxis};
 
-pub trait Algorithm {
+pub trait Algorithm<D: Dimension + RemoveAxis> {
+    fn fit<I: Data<Elem = f64>>(x: ArrayBase<I,D>) -> Self;
+
     fn query<I: Data<Elem=f64>, Dim: Dimension + RemoveAxis>(&self, x: &ArrayBase<I, Dim>) -> Vec<f64>;
 }
 
@@ -18,7 +20,13 @@ impl<D: Dimension + RemoveAxis> Brute<D> {
     }
 }
 
-impl<D: Dimension + RemoveAxis> Algorithm for Brute<D> {
+impl<D: Dimension + RemoveAxis> Algorithm <D> for Brute<D> {
+    fn fit<I: Data<Elem = f64>>(x: ArrayBase<I, D>) -> Self {
+        Brute {
+            data: x.into_owned()
+        }
+    }
+
     fn query<I, Dim>(&self, x: &ArrayBase<I, Dim>) -> Vec<f64>
         where
             I: Data<Elem=f64>,
@@ -45,4 +53,33 @@ fn calculate_euclidan<A, D, E>(point1: &ArrayBase<A, D>, point2: &ArrayBase<A, E
         .map(|(&elem2, &elem1)| {
             (elem2 - elem1).powi(2)
         }).sum::<f64>().sqrt()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::nearest_neighbors::*;
+
+    #[test]
+    fn brute_force() {
+        let other_test = array![[1., 2.], [3. , 4.], [5., 6.]];
+        let sample_input = array![[1., 1.], [5., 6.]];
+
+        let algo = Brute::new(other_test);
+        let distances = algo.query(&sample_input);
+        assert_eq!(distances, [1.0, 3.605551275463989, 6.4031242374328485, 5.656854249492381, 2.8284271247461903, 0.0]);
+    }
+
+    //    #[test]
+//    fn ball_tree() {
+//        let other_test = array![[1., 2.], [3. , 4.], [5., 6.]];
+//
+//        let parameters = NearestNeighborsHyperParameters::new().build();
+//        let nbr = NearestNeighbors::new(BallTree::default(),parameters).fit(other_test);
+//
+//        let sample_input = array![[1., 1.], [5., 6.]];
+//        let distances = nbr.kneighbors(sample_input.into_dyn());
+//        println!("Distances: {:?}", distances);
+//
+//        assert_eq!(2 + 2, 4);
+//    }
 }
--- linfa-neighbors/src/nearest_neighbors/mod.rs
+++ linfa-neighbors/src/nearest_neighbors/mod.rs
@@ -6,6 +6,7 @@ mod hyperparameters;
 
 pub use algorithm::*;
 pub use hyperparameters::*;
+use std::marker::PhantomData;
 
 pub enum AlgorithmType {
     BallTree,
@@ -13,27 +14,52 @@ pub enum AlgorithmType {
     KDTree,
 }
 
-pub struct NearestNeighbors<A: Algorithm> {
+pub struct NearestNeighbors<A: Algorithm<Dim>, Dim: Dimension + RemoveAxis> {
     algorithm: A,
+    dimension: std::marker::PhantomData<Dim>,
 }
 
-impl<A: Algorithm> NearestNeighbors<A> {
-    pub fn new(algorithm: A, _hyperparameters: NearestNeighborsHyperParameters) -> NearestNeighbors<A> {
+impl<A, Dim> NearestNeighbors<A, Dim>
+    where
+        Dim: Dimension + RemoveAxis,
+        A: Algorithm<Dim>,
+{
+    pub fn new(algorithm: A, _hyperparameters: NearestNeighborsHyperParameters) -> NearestNeighbors<A, Dim> {
         NearestNeighbors {
-            algorithm
+            algorithm,
+            dimension: PhantomData,
         }
     }
 
-    pub fn fit<I: Data<Elem=f64>, D: Dimension + RemoveAxis>(mut self, input: ArrayBase<I, D>) -> Self {
-//        self.algorithm.fit(input);
+    pub fn fit<I: Data<Elem=f64>, D: Dimension + RemoveAxis>(mut self, input: ArrayBase<I, D>) -> Self
+        where
+            A: Algorithm<D>
+    {
+        self.algorithm = Algorithm::fit(input);
         self
     }
 
-    pub fn kneighbors<I, D>(&self, x: ArrayBase<I, D>) -> Vec<f64>
+    pub fn kneighbors<I>(&self, x: ArrayBase<I, Dim>) -> Vec<f64>
         where
             I: Data<Elem=f64>,
-            D: Dimension + RemoveAxis
     {
         self.algorithm.query(&x)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::nearest_neighbors::*;
+
+    #[test]
+    fn nearest_neighbor_brute_force() {
+        let other_test = array![[1., 2.], [3. , 4.], [5., 6.]];
+        let sample_input = array![[1., 1.], [5., 6.]];
+
+        let parameters = NearestNeighborsHyperParameters::new().build();
+        let nbr = NearestNeighbors::new(Brute::<Ix2>::default(),parameters).fit(other_test);
+        let distances = nbr.kneighbors(sample_input);
+        assert_eq!(distances, [1.0, 3.605551275463989, 6.4031242374328485, 5.656854249492381, 2.8284271247461903, 0.0]);
     }
 }
